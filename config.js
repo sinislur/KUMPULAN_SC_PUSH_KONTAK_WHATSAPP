480c146c035dc6a25f6a4d6fd0f5496a866310a3
kintolgede60 <kintolgede60@gmail.com>
2023-05-21 14:06:14 +0700

    work

---
+++ config.js
@@ -0,0 +1,32 @@
+const chalk = require("chalk")
+const fs = require("fs")
+
+global.ownerNumber = ["6289688206739@s.whatsapp.net"]
+global.nomerOwner = "6289688206739"
+global.nomorOwner = ['6289688206739']
+global.nameGEDE = "KIRBOTZ"
+global.namaDeveloper = "KirBotz"
+global.namaBot = "KirBotz WhatsApp"
+global.packname = ""
+global.author = "Sticker By KirBotz"
+global.thumb = fs.readFileSync("./thumb.png")
+
+let file = require.resolve(__filename) 
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(chalk.redBright(`Update ${__filename}`))
+delete require.cache[file]
+require(file)
+})
+
+/*
+
+Thanks To By KirBotz
+Base Ori By KirBotz
+Ubah Nomor Owner?
+Ganti Di File ./owner.json
+Harap Jangan Jual Sc Ini
+Karena Sc Ini Free Langsung Dari
+Youtube : https://youtube.com/@kangbotwhatsapp
+
+*/
